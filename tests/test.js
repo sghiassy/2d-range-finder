d7f6058ad60202a72b0fa73770a296894cb6d265
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-09-28 00:46:18 -0700

    Another test added

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,12 @@
 var should = require('should');
+var testFile = require('../src/temp');
 
 describe("testing", function() {
   it("should work", function() {
     ("test").should.equal("test");
+  });
+
+  it("can import a file", function() {
+    testFile.should.equal("It works from content.js2222.");
   });
 });
