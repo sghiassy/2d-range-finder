6ee5d2ad941cd5bc9bf15081a5181d6276efae9e
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-09-28 02:58:57 -0700

    Added Styling

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,11 +1,39 @@
 var React = require('react');
 
-export default React.createClass({
+var Greeting = React.createClass({
+  getInitialState: function() {
+    return {secondsElapsed: 0};
+  },
+
   render: function() {
     return (
       <div className="greeting">
+        <h1 style={Style.header}>Whoa</h1>
         Hello, {this.props.name}!
+        <p>{this.state.secondsElapsed}</p>
       </div>
     );
   },
+
+  /**
+   * Timer methods
+   */
+
+  tick: function() {
+    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+  },
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
 });
+
+var Style = {
+  header: {
+    color: 'red'
+  }
+}
+
+module.exports = Greeting;
