3ec87a89669f9c9d1b1011c4ae9310e9e4c8440d
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-09-27 23:20:03 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// require the core node events module
+var EventEmitter = require('events').EventEmitter;
+var txt = require('./temp');
+
+//create a new event emitter
+var emitter = new EventEmitter;
+
+// set up a listener for the event
+emitter.on('pizza', function(message){
+  console.log(message);
+});
+
+// emit an event
+emitter.emit('pizza', 'pizza is extremely yummy');
+
+document.write(txt);
+
+
+require("./styles.css");
