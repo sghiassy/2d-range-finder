9de5047d1a1e1b52543edb50e4be04d1284d0aff
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-09-28 02:48:13 -0700

    Moar

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var React = require('react');// require the core node events module
 var Greeting = require('./src/Greeting');
+var styles = require('./src/styles.css');
 
 React.render(
   <Greeting name="World"/>,
