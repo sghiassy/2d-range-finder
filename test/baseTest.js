4f73dc1e6292fc65c93932c03b20a49009cd2ffe
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-10-10 11:54:30 -0700

    Base Test Working

---
+++ test/baseTest.js
@@ -0,0 +1,6 @@
+describe("Base Assumption", function() {
+  it("Mocha and UnitJS should be working", function() {
+    (2).should.equal(2);
+    (2).should.not.equal(3);
+  });
+});
---
+++ test/titleSpec.js
@@ -0,0 +1,18 @@
+// 'use strict';
+//
+// import React from 'react/addons';
+// import Arist from '../src/title';
+//
+// var TestUtils = React.addons.TestUtils;
+//
+// describe('Artist', ()=>{
+//   var component;
+//
+//   beforeEach(() => {
+//     component = TestUtils.renderIntoDocument(<Title displayName="testingy" />);
+//   });
+//
+//   it("should display the correct name", ()=>{
+//     expect(component.getDOMNode().textContent.toMatch('testingy'));
+//   });
+// });
