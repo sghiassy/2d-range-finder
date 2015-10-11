6c6fe0f42ca859780cf2358f1df14d41f0171f57
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-10-10 17:46:08 -0700

    Cleaning

--- src/canvas/app.js
+++ src/canvas/app.js
@@ -5,10 +5,10 @@ var ClickStateMachine = require('./clickStateMachine');
 class App {
   constructor(props) {
     this.el = props.el;
+
+    // Setup Draw Context
     this.ctx = props.ctx;
-    // this.ctx.beginPath();
-    // this.ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-    // this.ctx.stroke();
+    this.ctx.font = "10px Arial";
 
     this.clickState = new ClickStateMachine();
   }
@@ -27,20 +27,44 @@ class App {
         y: orig.y
       };
 
-      this.ctx.beginPath();
-      this.ctx.moveTo(orig.x, orig.y);
-      this.ctx.lineTo(end.x, end.y);
-      this.ctx.stroke();
+      this.drawLine({
+        start: {x: orig.x, y:orig.y},
+        end: {x: end.x, y:end.y}
+      });
+
+      this.drawLineLabel({
+        start: {x: orig.x, y:orig.y},
+        end: {x: end.x, y:end.y}
+      });
 
       this.temp = undefined;
     } else {
       this.temp = {x:evt.pageX,y:evt.pageY};
     }
-    // this.ctx.beginPath();
-    // this.ctx.arc(evt.pageX - this.el.getBoundingClientRect().left,
-    //              evt.pageY - this.el.getBoundingClientRect().top,
-    //              40, 0, 2 * Math.PI);
-    // this.ctx.stroke();
+  }
+
+  drawLine(coord) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(coord.start.x, coord.start.y);
+    this.ctx.lineTo(coord.end.x, coord.end.y);
+    this.ctx.stroke();
+  }
+
+  drawLineLabel(coord) {
+    var startingPointTxt = "{x:" + coord.start.x + ", y:" + coord.start.y + "}";
+    var endingPointTxt = "{x:" + coord.end.x + ", y:" + coord.end.y + "}";
+
+    var labelWidth = this.ctx.measureText(startingPointTxt).width + this.ctx.measureText(endingPointTxt).width;
+    var lineLength = coord.end.x - coord.start.x;
+    var lineIsTooShort = labelWidth >= lineLength;
+
+    if (!lineIsTooShort) {
+      this.ctx.fillText(startingPointTxt, coord.start.x, coord.start.y-5);
+      this.ctx.fillText(endingPointTxt, coord.end.x - this.ctx.measureText(endingPointTxt).width, coord.end.y-5);
+    } else {
+      var shortenedLabel = "{x:" + coord.start.x + ", y:" + coord.start.y + " x:" + coord.end.x + "}";
+      this.ctx.fillText(shortenedLabel, coord.start.x, coord.start.y-5);
+    }
   }
 }
 
